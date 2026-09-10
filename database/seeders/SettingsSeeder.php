7ac335474f90dc277c6e69c4628f4773e50b3d8b
Chiheb <chihebelhajali@gmail.com>
2026-09-10 20:18:03 +0100

    Fix duplicated settings seeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -2,34 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Setting;
-
-class SettingsSeeder extends Seeder
-{
-    public function run()
-    {
-        $defaults = [
-            'commune_name' => 'My Commune',
-            'commune_address' => null,
-            'commune_phone' => null,
-            'commune_email' => null,
-            'commune_logo_path' => null,
-            'working_hours' => json_encode([]),
-            'is_service_birth_certificate_enabled' => '1',
-            'is_service_permit_enabled' => '1',
-            'smtp_test_recipient' => null,
-        ];
-
-        foreach ($defaults as $k => $v) {
-            Setting::updateOrCreate(['key' => $k], ['value' => $v]);
-        }
-    }
-}
-<?php
-
-namespace Database\Seeders;
-
 use App\Models\Setting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
